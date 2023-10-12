81b0dbac6112d5f5bf4abfdf83391b1f2e73bd79
ankitkumardev25 <ankitkumar1999825@gmail.com>
2023-10-12 22:23:15 +0530

    joke-bot

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,46 @@
 import { useEffect, useState } from 'react'
+import axios from 'axios'
+
 import './App.css'
 
 function App() {
-  const jokeAPI =
-    'https://official-joke-api.appspot.com/jokes/programming/random'
-  const [joke, setJoke] = useState(false)
+  const [selectedVoice, setSelectedVoice] = useState(147)
+  const [joke, setJoke] = useState(null)
+  const [loading, setLoading] = useState(false)
 
-  useEffect(async () => {
-    const response = await fetch(jokeAPI)
-    const newJoke = await response.json()
-    console.log(newJoke)
-  }, [joke])
+  async function getJoke() {
+    const { data } = await axios.get(
+      'https://official-joke-api.appspot.com/jokes/programming/random'
+    )
+    return data[0]
+  }
+
+  const tellMeAJoke = async () => {
+    setLoading(true)
+    const joke = await getJoke()
+    setJoke(joke)
+    speak(`${joke.setup} ${joke.punchline} ja ja ja`, selectedVoice)
+    setLoading(false)
+  }
+
+  useKeyDown(() => tellMeAJoke(), ['j'])
 
   return (
-    <>
-      <div className="card">
-        <button onClick={() => setJoke((joke) => true)}>Tell me a JOKE</button>
-      </div>
-    </>
+    <div className="center">
+      <img src={require('./robot.gif')} alt="robot" />
+      <VoiceSelector selected={selectedVoice} setSelected={setSelectedVoice} />
+      {!!joke && (
+        <div>
+          <p>{joke.setup}</p>
+          <p>{joke.punchline}</p>
+        </div>
+      )}
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <button onClick={tellMeAJoke}>Tell me a joke!</button>
+      )}
+    </div>
   )
 }
 
